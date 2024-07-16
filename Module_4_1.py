ce700b360ed888c0c038b7cd9ec01fe9ec636358
Mik-user <mikmikh@yandex.ru>
2024-07-16 21:26:16 +0300

    Add files via upload

---
+++ Module_4_1.py
@@ -0,0 +1,10 @@
+import Fake_math as Fake
+import True_math as Tr
+result1 = Fake.divide(46767679, 58)
+result2 = Fake.divide(98, 0)
+result3 = Tr.divide(760, 5)
+result4 = Tr.divide(17, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
